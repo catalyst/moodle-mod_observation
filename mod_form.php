48ccf9e27966bdf991651b27d7887e80890203b4
Jared Hungerford <56417228+Gerrrrad@users.noreply.github.com>
2021-05-04 08:56:41 +1000

    Add files via upload
    Added time slots to be customized

--- mod_form.php
+++ mod_form.php
@@ -17,14 +17,13 @@
 /**
  * Defines the observation module settings form.
  *
- * @package    mod_observation
- * @copyright  2021 Endurer Solutions Team
- * @author Matthew Hilton <mj.hilton@outlook.com>
+ * @package    mod_form
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
-require_once($CFG->dirroot.'/course/moodleform_mod.php');
+require_once($CFG->dirroot . '/course/moodleform_mod.php');
+// require_once($CFG->dirroot . '/mod/quiz/locallib.php');
 
 /**
  * Settings form for the observation module.
@@ -32,21 +31,40 @@ require_once($CFG->dirroot.'/course/moodleform_mod.php');
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class mod_observation_mod_form extends moodleform_mod {
-    /**
-     * Called to define the form for this observation instance.
-     * @return void
-     */
+    public function __construct($current, $section, $cm, $course){
+        // Can do things to add extra data to constructor here
+        // Else just call the parents constructor 
+        parent::__construct($current, $section, $cm, $course);
+    }
+
     protected function definition() {
-        // Get config and get the form object to construct.
+        // Get config and get the form object to construct
         $obsconfig = get_config('observation');
         $mform = $this->_form;
 
-        // General.
-        $mform->addElement('header', 'general', get_string('general', 'form'));
-        $mform->addElement('text', 'name', get_string('name'), array('size' => '64'));
+        // General
+        $mform->addElement('header', 'general', get_string('general', 'observation'));
+        $mform->addElement('text', 'name', get_string('name'), array('size'=>'64'));
 
-        // Footer.
+        // Time Slots
+        $mform->addElement('header', 'timeslot', get_string('timeslot', 'observation'));
+
+        //Testing
+        //$mform->addElement('text', 'starttime', get_string('starttime', 'observation'), array('size'=>'64'));
+        $mform->setExpanded('timeslot', true);
+
+        //Setting up boxes to set time slots
+        $name = get_string('starttime', 'observation');
+        $mform->addElement('date_time_selector', 'starttime', $name, array('optional'=>false));
+        $mform->addHelpButton('starttime', 'starttime', 'observation');
+
+        $name = get_string('endtime', 'observation');
+        $mform->addElement('date_time_selector', 'endtime', $name, array('optional'=>false));
+        $mform->addHelpButton('endtime', 'endtime', 'observation');
+
+        //-------------------------------------------------------
         $this->standard_coursemodule_elements();
+        //-------------------------------------------------------
         $this->add_action_buttons();
     }
 }
