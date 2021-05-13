72282c5380c0696dbe9cf4c9892df643dcb93f87
RePackk <80939207+RePackk@users.noreply.github.com>
2021-05-13 19:46:51 +1000

    Update mod_form.php

--- mod_form.php
+++ mod_form.php
@@ -71,7 +71,7 @@ class mod_observation_mod_form extends moodleform_mod {
         //    'multiple' => false,
         //    'noselectionstring' => get_string('allareas', 'search'),
         //);
-        $mform->addElement('header', 'Selecting-Observer', get_string('Selecting Observer', 'observation');
+        $mform->addElement('header', 'Selecting-Observer', get_string('Selecting Observer', 'observation'));
         $mform->addElement('autocomplete', 'observers', get_string('Select Observer', 'observation'), get_string('searcharea', 'search'), $areanames);
         
 
