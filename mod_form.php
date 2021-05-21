8364b14ddf6960738c51372b774c4e2cbebae24d
Jared Hungerford <56417228+Gerrrrad@users.noreply.github.com>
2021-05-21 22:58:48 +1000

    Add files via upload

--- mod_form.php
+++ mod_form.php
@@ -19,7 +19,7 @@
  *
  * @package    mod_observation
  * @copyright  2021 Endurer Solutions Team
- * @author Matthew Hilton <mj.hilton@outlook.com>
+ * @author Matthew Hilton <mj.hilton@outlook.com>, Celine Lindeque
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -37,13 +37,20 @@ class mod_observation_mod_form extends moodleform_mod {
      * @return void
      */
     protected function definition() {
-        // Get config and get the form object to construct.
-        $obsconfig = get_config('observation');
         $mform = $this->_form;
 
         // General.
         $mform->addElement('header', 'general', get_string('general', 'observation'));
         $mform->addElement('text', 'name', get_string('name'), array('size' => '64'));
+        $mform->setType('name', PARAM_RAW);
+
+        // Activity Instructions Elements.
+        $mform->addElement('header', 'instructions', get_string('instructions', 'observation'));
+        $mform->addElement('editor', 'observerins_editor', get_string('instructionsobserver', 'observation'));
+        $mform->setType('observerins_editor', PARAM_RAW);
+
+        $mform->addElement('editor', 'observeeins_editor', get_string('instructionsobservee', 'observation'));
+        $mform->setType('observeeins_editor', PARAM_RAW);
 
         // Setting up boxes to set time slots.
         $mform->addElement('header', 'timeslot', get_string('timeslot', 'observation'));
@@ -55,9 +62,49 @@ class mod_observation_mod_form extends moodleform_mod {
         $mform->addElement('date_time_selector', 'endtime', $name, array('optional' => false));
         $mform->addHelpButton('endtime', 'endtime', 'observation');
 
+        // Observation Points.
+        $mform->addElement('header', 'observationpoints', get_string('observationpoints', 'observation'));
+        // Actions.
+        // Observation points.
+        // TODO: replace.
+        $mform->addElement('advcheckbox', 'placeholders', get_string('placeholder', 'observation'),
+        'Example: Using correct PPE while handling chemicals');
+        $mform->addElement('advcheckbox', 'placeholders', get_string('placeholder', 'observation'),
+        'Example: Reading chemical safety information');
+        $mform->addElement('advcheckbox', 'placeholders', get_string('placeholder', 'observation'),
+        'Example: Choosing correct equipment');
+
+        // Settings.
+
         // Footer.
         $this->standard_coursemodule_elements();
         $this->add_action_buttons();
     }
-}
 
+    /**
+     * Overrides some default values for some form elements
+     *
+     * @param array $defaultvalues Form defaults
+     * @return void
+     **/
+    public function data_preprocessing(&$defaultvalues) {
+        global $DB;
+
+        // Get observation instruction editor values from the db.
+        $obsid = $this->_instance;
+
+        // Creating new - no defaults to add.
+        if ($obsid == null) {
+            return;
+        }
+
+        // Editing, find the defaults and update the form values.
+        $obsdata = $DB->get_record('observation', array('id' => $obsid));
+
+        $defaultvalues['observerins_editor']->text = $obsdata->observer_ins;
+        $defaultvalues['observerins_editor']->format = $obsdata->observer_ins_f;
+        $defaultvalues['observeeins_editor']->text = $obsdata->observee_ins;
+        $defaultvalues['observeeins_editor']->format = $obsdata->observee_ins_f;
+        return;
+    }
+}
