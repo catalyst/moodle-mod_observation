651f089a4080a2a1002a0b0e8f25fcce653002fb
RePackk <Jack@Kepper.net>
2021-08-19 12:45:57 +1000

    Update timeslot_manager.php

--- classes/timeslot_manager.php
+++ classes/timeslot_manager.php
@@ -67,12 +67,6 @@ class timeslot_manager {
             }
         }
 
-        if (property_exists($data, 'observee_id')) {
-            if ($data->observee_id != null) {
-                throw new \coding_exception("Timeslot has been taken.");
-            }
-        }
-
         if ($newinstance) {
             $slotid = $DB->insert_record($tablename, $data, true);
             self::update_timeslot_calendar_events($data->obs_id, $slotid);
