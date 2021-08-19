fe812be0bfd7bcb498747412e62bd91760c86080
Celine <celinelindeque@gmail.com>
2021-08-19 13:23:46 +1000

    removed whitespace

--- classes/timeslots/timeslots_table.php
+++ classes/timeslots/timeslots_table.php
@@ -142,9 +142,9 @@ class timeslots_table extends \table_sql implements \renderable {
         }
 
         if ($this->displaymode === \mod_observation\timeslots\timeslots::DISPLAY_MODE_VIEW_ASSIGNED) {
-            $htmlout = $this->action_button('timesloteditor.php?mode=edit&', $row->obs_id, $row->id, 'edit', 
+            $htmlout = $this->action_button('timesloteditor.php?mode=edit&', $row->obs_id, $row->id, 'edit',
             get_string('edit', 'observation'));
-            $htmlout .= $this->action_button('timesloteditor.php?', $row->obs_id, $row->id, 'delete', 
+            $htmlout .= $this->action_button('timesloteditor.php?', $row->obs_id, $row->id, 'delete',
             get_string('delete', 'observation'));
         }
 
