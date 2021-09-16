17241a26f19d3fb1cf3f4b0afddc46c1457d81e4
Celine <celinelindeque@gmail.com>
2021-09-16 14:36:34 +1000

    added nonfunctional filter file for html filtering

--- classes/observation_manager.php
+++ classes/observation_manager.php
@@ -382,7 +382,7 @@ class observation_manager {
 
         $table->data = array_map(function($item) {
             // if $item->response is an image (change setType in form/pointmarking?) 
-            // then do the data['link'] url thing with img tags around it to put in the table
+            // then $item->response = do the data['link'] url thing with img tags around it to put in the table
             return [
                 $item->title,
                 $item->response,
